9e0b351712913033a0f40e51e988a871a23f9bc2
Shahan <shahan.khan@signit.sa>
2026-05-29 16:30:17 +0500

    feat: initial LazyVim nvim config
    - Base LazyVim setup with lazy.nvim plugin manager
    - jk → Escape keymap in insert mode
    - Tokyonight colorscheme with full transparency (sidebars, floats)
    - Toggle transparency with <leader>bg
    - Keymaps, options, and autocmds config stubs

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+-- Toggle background transparency
+local transparent = true
+vim.keymap.set("n", "<leader>bg", function()
+  transparent = not transparent
+  require("tokyonight").setup({ transparent = transparent, styles = { sidebars = transparent and "transparent" or "dark", floats = transparent and "transparent" or "dark" } })
+  vim.cmd("colorscheme tokyonight")
+end, { desc = "Toggle transparency" })
