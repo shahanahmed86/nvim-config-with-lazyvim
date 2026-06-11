51ab2c2255d02e61b4c518f3adf41ae2b1f64045
Shahan <shahan.khan@signit.sa>
2026-06-11 20:23:30 +0500

    feat: move explorer sidebar to the right side

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,6 +3,13 @@ return {
     "folke/snacks.nvim",
     opts = {
       dashboard = { enabled = true },
+      picker = {
+        sources = {
+          explorer = {
+            layout = { layout = { position = "right" } },
+          },
+        },
+      },
     },
   },
 }
