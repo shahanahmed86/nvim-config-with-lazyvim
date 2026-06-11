76f49ebfbce058c12cb3c336e5470d9da08baa8b
Shahan <shahan.khan@signit.sa>
2026-06-11 21:05:47 +0500

    feat: add <leader>qQ keymap to quit all without saving session

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -7,6 +7,7 @@ return {
       { "<leader>qs", function() require("persistence").load() end, desc = "Restore session" },
       { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore last session" },
       { "<leader>qd", function() require("persistence").stop() end, desc = "Don't save session" },
+      { "<leader>qQ", function() require("persistence").stop() vim.cmd("qa") end, desc = "Quit all without saving session" },
     },
   },
 }
